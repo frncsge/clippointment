3b722ee36549e846b9cc0f3830a9ed5d8cbe13b2
Francis Ge <francisge527@gmail.com>
2026-03-23 19:04:03 +0800

    run server on port 3000 locally and include default route

---
+++ server/index.js
@@ -0,0 +1,13 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("barber-appointment-system server is running");
+});
+
+app.listen(port, () => console.log(`Server is listening on port: ${port}`));

ead1817e7de324e66d40b3c0cc53d774d5eda07d
Francis Ge <francisge527@gmail.com>
2026-03-25 13:10:12 +0800

    insert new work hours after inputs are all valid and catch any errors

--- server/controllers/workHours.controller.js
+++ server/controllers/workHours.controller.js
@@ -1,6 +1,7 @@
 import { validateWorkHoursInput } from "../validators/workHours.validator.js";
+import { insertWorkHours } from "../models/workHours.model.js";
 
-export const addWorkHours = (req, res) => {
+export const addWorkHours = async (req, res) => {
   const { date, startTime, endTime, slotInterval } = req.body;
 
   const error = validateWorkHoursInput({
@@ -11,4 +12,25 @@ export const addWorkHours = (req, res) => {
   });
 
   if (error) return res.status(400).json({ message: error });
+
+  try {
+    await insertWorkHours({ date, startTime, endTime, slotInterval });
+    res.status(201).json({ message: "New work hours succesfully added" });
+  } catch (error) {
+    // error for duplicate values
+    if (error.code === '23505') {
+      return res.status(400).json({message: `Work hours for date ${date} already exist`})
+    }
+
+    console.error(
+      "An error occured while trying to add new work hours:",
+      error,
+    );
+    res
+      .status(500)
+      .json({
+        message:
+          "Server error. An error occured while trying to add new work hours.",
+      });
+  }
 };

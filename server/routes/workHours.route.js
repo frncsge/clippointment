636d01f9cea3f0282f8ae061f3900e9cc557e863
Francis Ge <francisge527@gmail.com>
2026-03-28 16:27:48 +0800

    add route for updating workhours

--- server/routes/workHours.route.js
+++ server/routes/workHours.route.js
@@ -2,13 +2,15 @@ import express from "express";
 import {
   addWorkHours,
   getWorkHours,
+  getAvailableTimeSlots,
+  updateWorkHours
 } from "../controllers/workHours.controller.js";
-import { getAvailableTimeSlots } from "../controllers/workHours.controller.js";
 
 const router = express.Router();
 
 router.post("/work-hours", addWorkHours);
 router.get("/work-hours/:date", getWorkHours);
 router.get("/work-hours/:date/time-slots", getAvailableTimeSlots);
+router.patch("/work-hours/:date", updateWorkHours);
 
 export default router;

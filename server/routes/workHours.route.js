52c0c9700100b9d15f44e4f53d4f8b6c2ed847db
Francis Ge <francisge527@gmail.com>
2026-03-29 13:11:45 +0800

    add route for deleting work hours by date

--- server/routes/workHours.route.js
+++ server/routes/workHours.route.js
@@ -3,16 +3,18 @@ import {
   addWorkHours,
   getWorkHours,
   getAvailableTimeSlots,
-  updateWorkHours
+  updateWorkHours,
+  deleteWorkHours,
 } from "../controllers/workHours.controller.js";
 import { addUnavailableTimeSlot } from "../controllers/unavailableTimeSlots.controller.js";
 
 const router = express.Router();
 
 router.post("/work-hours", addWorkHours);
-router.post("/work-hours/:date/unavailable-time-slots", addUnavailableTimeSlot)
+router.post("/work-hours/:date/unavailable-time-slots", addUnavailableTimeSlot);
 router.get("/work-hours/:date", getWorkHours);
 router.get("/work-hours/:date/time-slots", getAvailableTimeSlots);
 router.patch("/work-hours/:date", updateWorkHours);
+router.delete("/work-hours/:date", deleteWorkHours);
 
 export default router;

8d47e4bb4dd00627def80662ad120d74a60daac7
Francis Ge <francisge527@gmail.com>
2026-03-30 14:43:06 +0800

    add cooki-parser package

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 import workHoursRoutes from "./routes/workHours.route.js";
 import authRoutes from "./routes/auth.route.js";
 
@@ -9,6 +10,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api", authRoutes);
 app.use("/api", workHoursRoutes);

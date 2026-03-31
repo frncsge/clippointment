89e4544473525b9b1b171100a49d9d9b4b87eb5d
Francis Ge <francisge527@gmail.com>
2026-03-31 17:36:50 +0800

    create dynamic values clause for adding unavailable time slop to db query

--- server/models/unavailableTimeSlots.model.js
+++ server/models/unavailableTimeSlots.model.js
@@ -1,11 +1,17 @@
 import pool from "../../config/dbConfig.js";
 
-export const addUnavailableTimeSlotToDB = async (date, timeSlot) => {
+export const addUnavailableTimeSlotToDB = async (workHoursId, timeSlots) => {
   try {
-    await pool.query(
-      "INSERT INTO unavailable_time_slots (date, time_slot) VALUES ($1, $2)",
-      [date, timeSlot],
-    );
+    const valuesClause = timeSlots.map((_, i) => `($${i * 2 + 1}, $${i * 2 + 2})`).join(", ");
+
+    const values = timeSlots.flatMap((timeSlot) => [workHoursId, timeSlot]);
+
+    const query = `
+      INSERT INTO unavailable_time_slots (work_hours_id, time_slot)
+      VALUES ${valuesClause}
+    `;
+
+    await pool.query(query, values);
   } catch (error) {
     console.error(
       "An error occured while trying to add an unavailable time slot:",

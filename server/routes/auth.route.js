b81f44662884e630b2944c7b8f36a210ab0ebaee
Francis Ge <francisge527@gmail.com>
2026-04-01 21:01:18 +0800

    feat: auth route for refreshing jwt tokens

--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { logIn } from "../controllers/auth.controller.js";
+import { logIn, refresh } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
 router.post("/log-in", logIn);
+router.post("/refresh", refresh);
 
 export default router;

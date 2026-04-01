7a400fe310abc1d6b5e73c4b22a09e2a8b8e1842
Francis Ge <francisge527@gmail.com>
2026-04-01 21:01:57 +0800

    feat: add controller for refreshing jwt

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -5,6 +5,7 @@ import {
   generateRefreshToken,
 } from "../utils/token.util.js";
 import redisClient from "../../config/redisConfig.js";
+import jwt from "jsonwebtoken";
 
 export const logIn = async (req, res) => {
   const username = req.body.username?.trim();
@@ -65,5 +66,56 @@ export const logIn = async (req, res) => {
     res.status(500).json({
       message: "Server error. An error occured while trying to log in user.",
     });
+  }
+};
+
+export const refresh = async (req, res) => {
+  const { refreshToken } = req.cookies;
+
+  if (!refreshToken)
+    return res.status(401).json({ message: "No refresh token provided" });
+
+  try {
+    const decoded = jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET);
+
+    // revoke used refresh token
+    res.clearCookie("refreshToken", refreshToken, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "Strict",
+      maxAge: 15 * 60 * 1000,
+    });
+    await redisClient.del(`refreshToken:${refreshToken}`);
+
+    // generate new access token
+    const newAccessToken = generateAccessToken(decoded.sub);
+
+    // generate new refresh token and store it in redis with 7-day expiration
+    const newRefreshToken = generateRefreshToken(decoded.sub);
+    const sevenDaysInSecs = 7 * 24 * 60 * 60;
+    await redisClient.setEx(
+      `refreshToken:${decoded.sub}`,
+      sevenDaysInSecs,
+      newRefreshToken,
+    );
+
+    // send new access and refresh tokens
+    res.cookie("accessToken", newAccessToken, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "Strict",
+      maxAge: 15 * 60 * 1000,
+    });
+
+    res.cookie("refreshToken", newRefreshToken, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "Strict",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+
+    res.status(201).json({ message: "Access token refreshed" });
+  } catch (error) {
+    res.status(401).json({ message: "Invalid or expired refresh token" });
   }
 };

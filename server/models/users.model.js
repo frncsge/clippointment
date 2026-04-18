f4588084fa191ba5f92e821da120f4a7f462b9ea
Francis Ge <francisge527@gmail.com>
2026-04-18 15:17:29 +0800

    refactor: change getUserByUsername to getUserByEmail

--- server/models/users.model.js
+++ server/models/users.model.js
@@ -1,15 +1,15 @@
 import pool from "../../config/dbConfig.js";
 
-export const getUserByUsername = async (username) => {
+export const getUserByEmail = async (email) => {
   try {
-    const result = await pool.query("SELECT * FROM users WHERE username = $1", [
-      username,
+    const result = await pool.query("SELECT * FROM users WHERE email = $1", [
+      email,
     ]);
 
     return result;
   } catch (error) {
     console.error(
-      "An error occured while trying to get user by username:",
+      "An error occured while trying to get user by email:",
       error,
     );
     throw error;

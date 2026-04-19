c7d8a918a05375933b193e60accb547d9db4ae83
Francis Ge <francisge527@gmail.com>
2026-04-19 15:02:55 +0800

    feat: add controller for registering new user

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,37 +1,37 @@
 import bcrypt from "bcrypt";
-import { getUserByEmail } from "../models/users.model.js";
+import { getUserByEmail, storeNewUser } from "../models/users.model.js";
 import {
   generateAccessToken,
   generateRefreshToken,
+  generateToken,
 } from "../utils/token.util.js";
+import { isValidEmail } from "../utils/email.util.js";
+import { isValidPassword, passwordsMatch } from "../utils/password.util.js";
+import { sendVerificationEmail } from "../utils/email.util.js";
 import redisClient from "../../config/redisConfig.js";
 import jwt from "jsonwebtoken";
+
+const saltRound = 12;
 
 export const logIn = async (req, res) => {
   const email = req.body.email?.trim();
   const password = req.body.password?.trim();
 
   if (!email || !password)
-    return res
-      .status(400)
-      .json({ message: "Email and password are required" });
+    return res.status(400).json({ message: "Email and password are required" });
 
   try {
     const user = await getUserByEmail(email);
 
     if (user.rowCount === 0)
-      return res
-        .status(401)
-        .json({ message: "Incorrect email or password" });
+      return res.status(401).json({ message: "Incorrect email or password" });
 
     // check password
     const hashedPassword = user.rows[0].hashed_password;
     const match = await bcrypt.compare(password, hashedPassword);
 
     if (!match)
-      return res
-        .status(401)
-        .json({ message: "Incorrect email or password" });
+      return res.status(401).json({ message: "Incorrect email or password" });
 
     // if match, create session
     const userId = user.rows[0].id;
@@ -65,6 +65,76 @@ export const logIn = async (req, res) => {
     console.error("An error occured while trying to log in user:", error);
     res.status(500).json({
       message: "Server error. An error occured while trying to log in user.",
+    });
+  }
+};
+
+export const register = async (req, res) => {
+  const email = req.body.email?.trim();
+  const password = req.body.password?.trim();
+  const confirmPassword = req.body.confirmPassword?.trim();
+  const accountName = req.body.accountName?.trim();
+
+  // empty values not allowed
+  if (!email || !password || !confirmPassword || !accountName)
+    return res.status(400).json({
+      message: "All inputs are required",
+      requiredInputs: ["email", "password", "confirmPassword", "accountName"],
+    });
+
+  // example of invalid: user email@domain.com (space inside)
+  if (!isValidEmail(email))
+    return res.status(400).json({ message: "Invalid email format" });
+
+  // validate password
+  if (!isValidPassword(password))
+    return res
+      .status(400)
+      .json({ message: "Password must be at least 8 characters long" });
+
+  if (!passwordsMatch(password, confirmPassword))
+    return res.status(400).json({ message: "Passwords do not match" });
+
+  try {
+    // check if user already exists
+    const user = await getUserByEmail(email);
+
+    if (user.rowCount > 0)
+      return res.status(400).json({ message: "Email already registered" });
+
+    // hash password
+    const hashedPassword = await bcrypt.hash(password, saltRound);
+
+    // store new user in db with is_verified col equal to false (need email verification)
+    const result = await storeNewUser({ email, hashedPassword, accountName });
+    const { id: userId } = result.rows[0];
+
+    // generate token for verification
+    const verificationToken = generateToken();
+
+    // store token in redis with 10 mins TTL
+    await redisClient.setEx(
+      `verify:${verificationToken}`,
+      10 * 60,
+      String(userId),
+    );
+
+    // send email verification link
+    const link = `dummylink/verify?token=${verificationToken}`;
+    sendVerificationEmail(email, link).catch(console.error);
+
+    res
+      .status(200)
+      .json({ message: "Verifiaction email sent. Check your inbox" });
+  } catch (error) {
+    // duplicate email (for idempotency)
+    if (error.code === "23505")
+      return res.status(400).json({ message: "Email already resgistered" });
+
+    console.error("An error occured while trying to register new user:", error);
+    res.status(500).json({
+      message:
+        "Server error. An error occured while trying to register new user.",
     });
   }
 };

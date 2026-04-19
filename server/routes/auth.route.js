65e4348de5fdb1c90d99bcff65e424955e6d0d77
Francis Ge <francisge527@gmail.com>
2026-04-19 15:04:27 +0800

    feat: add route for registering new user

--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { logIn, refresh, logOut } from "../controllers/auth.controller.js";
+import { logIn, refresh, logOut, register } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
+router.post("/register", register);
 router.post("/log-in", logIn);
 router.post("/log-out", logOut);
 router.post("/refresh", refresh);

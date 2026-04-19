fc77bc2553277ae732b02820e557c237e33f035d
Francis Ge <francisge527@gmail.com>
2026-04-19 15:03:14 +0800

    feat: add function for storing new user in the db

--- server/models/users.model.js
+++ server/models/users.model.js
@@ -8,10 +8,21 @@ export const getUserByEmail = async (email) => {
 
     return result;
   } catch (error) {
-    console.error(
-      "An error occured while trying to get user by email:",
-      error,
+    console.error("An error occured while trying to get user by email:", error);
+    throw error;
+  }
+};
+
+export const storeNewUser = async ({ email, hashedPassword, accountName }) => {
+  try {
+    const result = await pool.query(
+      "INSERT INTO users (email, hashed_password, account_name) VALUES ($1, $2, $3) RETURNING id",
+      [email, hashedPassword, accountName],
     );
+
+    return result;
+  } catch (error) {
+    console.error("An error occured while trying to store new user:", error);
     throw error;
   }
 };

e56b0330c218f61771a91b036990bd6ac39696ea
Francis Ge <francisge527@gmail.com>
2026-04-20 14:12:03 +0800

    feat: add route for sending email verification links

--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { logIn, refresh, logOut, register, verify } from "../controllers/auth.controller.js";
+import { logIn, refresh, logOut, register, verify, sendVerification } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
@@ -7,6 +7,7 @@ router.post("/register", register);
 router.post("/log-in", logIn);
 router.post("/log-out", logOut);
 router.post("/refresh", refresh);
+router.post("/email-verifications", sendVerification);
 
 router.get("/email-verifications/:token", verify);
 
